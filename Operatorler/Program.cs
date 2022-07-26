aabab81befe1b341b41dc0453293ef73a6ec2615
Mustafa Tortuk <mustafat237@hotmail.com>
2022-07-26 21:33:15 +0300

    Operatorler
    Operatorler c#

---
+++ Operatorler/Program.cs
@@ -0,0 +1,64 @@
+﻿// Atama işlemi 
+int x = 3;
+int y = 3;
+y+=2;
+Console.WriteLine(y);
+y+=2;
+Console.WriteLine(y);
+y/=1;
+Console.WriteLine(y);
+x+=2;
+Console.WriteLine(x);
+
+// Mantıksal Operatörler
+// ||, &&, !
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+if(isSuccess && isCompleted){
+    Console.WriteLine("Perfect!");
+}
+if(isSuccess || isCompleted){
+    Console.WriteLine("Great!");
+}
+if(isSuccess && !isCompleted){
+    Console.WriteLine("Fine!");
+}
+
+// Ilişkisel Operatorler
+// <, >, <=, >=, ==, !=
+
+int a =3;
+int b =4;
+bool sonuc = a<b;
+
+Console.WriteLine(sonuc);
+sonuc = a>b;
+Console.WriteLine(sonuc);
+sonuc = a>=b;
+Console.WriteLine(sonuc);
+sonuc = a<=b;
+Console.WriteLine(sonuc);
+sonuc = a==b;
+Console.WriteLine(sonuc);
+sonuc = a!=b;
+Console.WriteLine(sonuc);
+
+// Aritmetiksel Operatorler
+// +, -, /, *, %
+
+int sayi1 = 10;
+int sayi2 = 5;
+int islem =  sayi1/sayi2;
+
+Console.WriteLine(islem);
+islem=sayi1*sayi2;
+Console.WriteLine(islem);
+islem=sayi1+sayi2;
+Console.WriteLine(islem);
+islem = sayi1++;
+Console.WriteLine(islem);
+
+int islem2 = 20%3;
+Console.WriteLine(islem2);
