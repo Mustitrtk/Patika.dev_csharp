b5109bbdcaa544498f690bebc76357ed3113ccd1
Mustafa Tortuk <mustafat237@hotmail.com>
2022-07-29 22:20:09 +0300

    HataYakalama
    Hata yakalama alıştırması c#

---
+++ HataYakalama/Program.cs
@@ -0,0 +1,19 @@
+﻿
+try{
+    int a = int.Parse(Console.ReadLine());
+}
+catch(FormatException e){
+    Console.WriteLine("Hatalı giris!!!");
+    Console.WriteLine(e.Message);
+}
+catch(ArgumentNullException e){
+    Console.WriteLine("Değer boş olamaz!!!");
+    Console.WriteLine(e.Message);
+}
+catch(ArgumentOutOfRangeException e){
+    Console.WriteLine("Değerin sınırlarını aştı!!!");
+    Console.WriteLine(e.Message);
+}
+finally{
+    Console.WriteLine("Bitiş.");
+}
