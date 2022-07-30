0e492fb6e7474cbb9db670d05ba812d9701b2c77
Mustafa Tortuk <mustafat237@hotmail.com>
2022-07-30 16:00:26 +0300

    AsalMi
    Girilen degerin asal olup olmadigini bulan c# programi

---
+++ AsalMi/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Bir sayi giriniz : ");
+int a = int.Parse(Console.ReadLine());
+bool asalMi= true;
+for(int i = 2; i<a;i++){
+    if(a%i==0) asalMi = false;
+}
+Console.WriteLine(asalMi==true?$"{a} sayisi asaldir. ":$"{a} sayisi asal degildir.");
