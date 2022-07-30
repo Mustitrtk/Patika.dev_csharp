536918aedfc800005c99cdbd3828b6b0f26a793b
Mustafa Tortuk <mustafat237@hotmail.com>
2022-07-30 13:58:51 +0300

    KararYapilari
    Karar yapilari c#

---
+++ KararYapilari/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+if(a%3==0 && a%5==0){
+    Console.WriteLine("FizzBuzz");
+}
+else if(a%3==0){
+    Console.WriteLine("Fizz");
+}
+else if(a%5==0){
+    Console.WriteLine("Buzz");
+}
+else Console.WriteLine(a);
