1e1e1df4f17ceae1e14a1d1f99c4ec2a6bddffff
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-02 14:36:34 +0300

    ArrayMethod
    Array methods in c#

---
+++ ArrayMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] arr = new int[]{1,2,5,8,0,11};
+
+Console.WriteLine("****Sort****");
+Array.Sort(arr);
+foreach(int num in arr){
+    Console.Write(num+" ");
+}
+Console.WriteLine();
+
+int enKucuk = arr[0];
+
+Console.WriteLine("****IndexOf****");
+int enKucukIndex = Array.IndexOf(arr,enKucuk);
+Console.WriteLine($"En kucuk eleman {enKucuk}, ve indexi {enKucukIndex}");
+
+Array.Reverse(arr);
+foreach(int num in arr){
+    Console.Write(num+" ");
+}
