05048e910aa3dec1e577cc5526deb60679000621
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-02 12:43:45 +0300

    SayiOrt
    While dongüsü ile sayi ortalamasi bulma c#

---
+++ SayiOrt/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter a number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sayac = 1;
+int total =0;
+while(sayac <= n){
+    total += sayac;
+    sayac++;
+}
+
+Console.WriteLine($"Avarage of the number {(double)total/n}");
+
+int[] numbers = new int[]{1,2,3,4,5,6,7,8};
+int total2 =0;
+int len = numbers.Length;
+foreach(int num in numbers){
+    total2+=num;
+}
+
+Console.WriteLine($"Avarage of the array {(double)total2/len}");
