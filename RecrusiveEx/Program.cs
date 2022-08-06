922b91bd64fea314463bac9d37852e775102f0eb
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-06 21:48:46 +0300

    RecrusiveEx
    Recrusive examples in c#

---
+++ RecrusiveEx/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(pow(3,4));
+    }
+
+    public static int faktoriyel(int n) {
+        if(n<1) return 1;
+        else return n*faktoriyel(n-1);
+    }
+
+    public static int pow(int a, int b){
+        if(b<2) return a;
+        
+        return a * pow(a,b-1);
+    }
+}
