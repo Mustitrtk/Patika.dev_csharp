2d9abca7e5acc704f3a7cf471b91185aff1b9b45
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-06 16:31:21 +0300

    Array_Sort
    Diziyi sıralayan metod c#

---
+++ Array_Sort/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[]{0,3,11,6,4};
+        sort(arr);
+        foreach(int item in arr){
+            Console.Write(item+" ");
+        }
+    }
+    public static void sort(int[] arr)
+    {
+        int temp = 1; ;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] < arr[j])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
