cf297ff2c02953cc0ef1140a7ddc53ad46197347
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-09 15:45:19 +0300

    Password
    Password

---
+++ Password/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a username : ");
+        string username = Console.ReadLine();
+        
+        string password="";
+        bool c = false;
+
+        while(c==false){
+
+            Console.Write("Enter a password : ");
+            password = Console.ReadLine();
+            c = control(password);
+            if(c==false) Console.WriteLine("Invalid password please try again.");
+
+        }
+
+        Console.WriteLine();
+
+        Console.Write("Password again : ");
+        string passwordA = Console.ReadLine();
+        if(password.CompareTo(passwordA)==0) Console.WriteLine("Password activeted!");
+        else Console.WriteLine("Error!");
+    }
+
+    public static bool control(string s)
+    {
+        bool c = false;
+        for(char i = '0',j='A';i<='9'&&j<='Z';i++,j++){
+            if(s.Contains(i)||s.Contains(j)) c = true;
+        }
+
+        return c;
+    }
+}
