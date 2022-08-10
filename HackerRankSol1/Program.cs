cc5d1fa55f8a33e8baf16aa258df1ab56f1563fa
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-10 12:45:45 +0300

    HackerRankSol1
    HackerRank solution

---
+++ HackerRankSol1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator myCalculator=new Calculator();
+
+        Console.Write("Enter first number :");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number :");
+        int p = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(myCalculator.power(n,p));
+    }
+}
+class Calculator{
+    public int power(int n, int p){
+        if(n<0||p<0) throw new System.Exception("n and p should be non-negative");
+        return (int) Math.Pow(n,p);
+    }
+}
