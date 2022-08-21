330ec0d89a248c53193013aebb2ef478ad5e0663
Mustafa Tortuk <mustafat237@hotmail.com>
2022-08-21 12:19:24 +0300

    ConstructorEx
    Constructor kullanim ornegi

---
+++ ConstructorEx/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Obs ogr = new Obs("213242017");
+        ogr.bilgiGoster();
+    }
+}
+
+class Obs{
+    private string ogrenciAd;
+    private string ogrenciSoyad;
+    private string ogrenciNo;
+
+    public Obs(string ad, string soyad, string no){
+        ogrenciAd = ad;
+        ogrenciSoyad = soyad;
+        ogrenciNo = no;
+    }
+
+    public Obs(string no){
+        ogrenciNo = no;
+    }
+
+    public void bilgiGoster(){
+        Console.WriteLine($"Ogrenci ad-soyad : {ogrenciAd}{ogrenciSoyad}");
+        Console.WriteLine($"Ogrenci no : {ogrenciNo}");
+    }
+}
