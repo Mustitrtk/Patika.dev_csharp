f1639615fb50d352752080e4b4cc5b386bc72b94
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-12 15:04:34 +0300

    Ucgen
    C# ucgen uyg.

---
+++ Ucgen/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Satir sayisi giriniz : ");
+        int satir = int.Parse(Console.ReadLine());
+        ucgen(satir);
+    }
+
+    public static void ucgen(int satir)
+    {
+        for(int i=satir;i>0;i--){
+            for(int j = i-1; j>0;j--){
+                Console.Write(" ");
+            }
+            for(int k = i; k<satir;k++){
+                Console.Write("*");
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
