56ac18584528b532569bc5f9e0091f995487ec87
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-12 16:21:03 +0300

    Alg
    Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartan c# kodu.

---
+++ Alg/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Lütfen kelime ve index giriniz (arada virgül ile) :  ");
+string str = Console.ReadLine();
+string[] metin = str.Split(",");
+int index = int.Parse(metin[1]);
+metin[0] = metin[0].Remove(index,1);
+Console.WriteLine(metin[0]);
