05602572fa6f460b689e6aed39e7f623bf18c492
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-12 14:46:36 +0300

    OrtHesap
    Fibonacci serisinde kullanicinin girdiği değere göre ortalama alma.

---
+++ OrtHesap/Program.cs
@@ -0,0 +1,17 @@
+﻿// Fibonacci serisinde kullanicinin girdiği değere göre ortalama alma.
+Console.Write("Satir degerini giriniz : ");
+int satir = int.Parse(Console.ReadLine()); 
+
+int a =1;
+int b =1;
+int c, ort=2;
+
+Console.WriteLine(a+"\n"+b);
+for(int i =0; i<satir;i++){
+    c=a+b;
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+    ort+=c;
+}
+Console.WriteLine("Ortalama : "+ort/satir);
