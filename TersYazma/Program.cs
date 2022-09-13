d71af05305872041d07871b931fa1e3c11a61f16
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-13 16:16:55 +0300

    TersYazma
    Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren c# uygulaması.

---
+++ TersYazma/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Cumle giriniz : ");
+        string str = Console.ReadLine();
+        string[] splt = str.Split(" ");
+
+        for(int i =0;i<splt.Length;i++){
+            rep(splt[i]);
+            Console.WriteLine();
+        }
+
+    }
+    public static void rep(string str)
+    {
+        char[] c = str.ToCharArray();
+        
+        for(int i = 1; i<c.Length;i++){
+            Console.Write(c[i]);
+        }
+        Console.Write(c[0]);
+    }
+}
