1e68c6d2a6b3d99a9e499aa118c97d6584d93533
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-15 15:56:27 +0300

    MutlakToplam
    Ekrandan girilen n tane sayının 67'den küçük yada büyük olduğuna bakan. Küçük olanların farklarının toplamını, büyük ise de farkların mutlak karelerini alarak toplayıp ekrana yazdıran c# programı.

---
+++ MutlakToplam/Program.cs
@@ -0,0 +1,18 @@
+﻿int kucuk=0,buyuk=0;
+
+Console.Write("Dizi boyutunu giriniz : ");
+int l = int.Parse(Console.ReadLine());
+
+
+for(int i =1;i<=l;i++){
+    Console.Write($"{i}. degeri giriniz : ");
+    int sayi = int.Parse(Console.ReadLine());
+    if(sayi>67){    
+        buyuk+=(int)(Math.Pow((sayi-67),2));
+    }
+    else{
+        kucuk+=(67-sayi);
+    }
+}
+
+Console.Write($"{kucuk} {buyuk}");
