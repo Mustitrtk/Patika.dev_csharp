c40ce2e9955f29b0deae61255a1c5183059306de
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-15 16:06:31 +0300

    KarakterDegistirme
    Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran c# programı.

---
+++ KarakterDegistirme/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Cumle giriniz : ");
+string mtn = Console.ReadLine();
+string[] metin = mtn.Split(" ");
+foreach(string item in metin){
+    char[] c = item.ToCharArray();
+    for(int i =1;i<c.Length;i++){
+        Console.Write(c[i]+"");
+    }
+    Console.Write(c[0]+" ");
+}
