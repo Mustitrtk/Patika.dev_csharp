bb7f0d1be0f38153944294a51a2c2aa5621e4fb3
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-15 14:49:33 +0300

    İkililerToplam
    Ekrandan girilen n tane integer ikililerin toplamını alan, eğer sayılar birbirinden farklıysa toplamlarını ekrana yazdıran, sayılar aynıysa toplamının karesini ekrana yazdıran c# uygulaması.

---
+++ IkililerToplam/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Dizi uzunlugunu giriniz : ");
+int l = int.Parse(Console.ReadLine());
+int[] ikiler = new int[l];
+for(int i =0;i<l;i++){
+    Console.Write($"Dizinin {i+1}. elemanini giriniz : ");
+    ikiler[i]=int.Parse(Console.ReadLine());
+}
+for(int j=0; j<l;j+=2){
+    if(l%2==1 && j+1==l) Console.Write(ikiler[j]);
+    else if(ikiler[j]!=ikiler[j+1]){
+        Console.Write((ikiler[j]+ikiler[j+1])+" ");
+    }
+    else{
+        Console.Write(Math.Pow((ikiler[j]+ikiler[j+1]),2)+" ");
+    }
+}
