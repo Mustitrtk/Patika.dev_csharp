0edc284fc6e1fcc9af66853ced38beb936eb67cf
Mustafa Tortuk <mustafat237@hotmail.com>
2022-09-18 11:12:47 +0300

    SessizHarf
    Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran c# programı.

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,25 @@
+﻿string unluler = "aeıioöuü";
+
+Console.Write("Metin giriniz : ");
+string text = Console.ReadLine();
+
+string[] kelimeler = text.Split(" ");
+
+
+foreach(string kelime in kelimeler){
+    int sayac =0;
+    bool control = false;
+    foreach(var harf in kelime){
+        if(unluler.Contains(harf)){
+            sayac =0;
+        }
+        else{
+            sayac++;
+            if(sayac ==2){
+                control = true;
+                break;
+            }
+        }
+    }
+    Console.Write(control+" ");
+}
